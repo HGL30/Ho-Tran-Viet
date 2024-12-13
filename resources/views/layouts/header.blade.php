40d5d164bec7221e526fa5034ec8f831101d663a
HGL30 <ht377465@gmail.com>
2024-12-13 19:54:42 +0700

    first-commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,12 @@
+<nav>
+    <div class="menu-function">
+        <ul>
+            <li><a href="{{ asset('/') }}">Trang chủ</a></li>
+            <li><a href="">Lịch sử</a></li>
+            <li><a href="">Phả hệ</a></li>
+            <li><a href="">Tin tức</a></li>
+            <li><a href="">Thông Tin Đời</a></li>
+            <li><a href="">Tài Nguyên</a></li>
+        </ul>
+    </div>
+</nav>
